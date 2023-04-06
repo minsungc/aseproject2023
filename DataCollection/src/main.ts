e1c080255ee4ce70559c35493a89f4cfb338533b
minsungc <minsung@ccs.neu.edu>
2023-04-06 20:16:32 +0000

    mathcomp prs

--- DataCollection/src/main.ts
+++ DataCollection/src/main.ts
@@ -14,9 +14,9 @@ dotenv.config();
 
 async function main() {
     // Language name and github token
-    const search = new Searcher('Lean', process.env.GH_TOKEN)
+    const search = new Searcher('Coq', process.env.GH_TOKEN)
     // owner, library name, https clone link
-    search.getPRList('leanprover-community', 'mathlib', 'https://github.com/leanprover-community/mathlib.git')
+    search.getPRList('math-comp', 'math-comp', 'https://github.com/math-comp/math-comp')
 }
 
 main()
