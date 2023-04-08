f7484835295fb2558c6d913bfc4ba2bb8d941efc
minsungc <minsung@ccs.neu.edu>
2023-04-08 23:02:26 +0000

    all relevant coq prs in coq-prs/

--- DataCollection/src/main.ts
+++ DataCollection/src/main.ts
@@ -17,17 +17,17 @@ dotenv.config();
 
 async function main() {
     // Language name and github token
-    const search = new Searcher('Lean', process.env.GH_TOKEN)
+    const search = new Searcher('Coq', process.env.GH_TOKEN)
     // owner, library name, https clone link
-    search.getPRList('leanprover-community', 'mathlib', 'https://github.com/leanprover-community/mathlib.git')
+    search.getPRList('coq', 'coq', 'https://github.com/coq/coq.git')
 }
 
-async function othermain() {
+// async function othermain() {
     // Language name and github token
-    const com = new CommitGetter('Lean', 'leanprover-community', 'mathlib', 'https://github.com/leanprover-community/mathlib.git')
-    await com.setup()
-    await com.getCommits()
-}
+    //const com = new CommitGetter('Lean', 'leanprover-community', 'mathlib', 'https://github.com/leanprover-community/mathlib.git')
+    //await com.setup()
+    //await com.getCommits()
+//}
 
 main()
 
