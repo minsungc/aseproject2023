797b1b716bffc415183b6d53e172678a815d011b
minsungc <minsung@ccs.neu.edu>
2023-04-09 17:38:00 +0000

    all coq commits

--- DataCollection/src/main.ts
+++ DataCollection/src/main.ts
@@ -7,7 +7,7 @@ import { Getter } from "./Getter"
 import fs from 'fs'
 
 // Comment out the one not being used
-import { Searcher } from "./MathLibraryPRs";
+//import { Searcher } from "./MathLibraryPRs";
 //import { Searcher } from "./MathLibraryISs"
 import { CommitGetter } from "./MathLibraryCMs"
 
@@ -15,19 +15,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-async function main() {
+// async function main() {
     // Language name and github token
-    const search = new Searcher('Coq', process.env.GH_TOKEN)
+    // const search = new Searcher('Coq', process.env.GH_TOKEN)
     // owner, library name, https clone link
-    search.getPRList('coq', 'coq', 'https://github.com/coq/coq.git')
+    // search.getPRList('coq', 'coq', 'https://github.com/coq/coq.git')
+// }
+
+async function othermain() {
+    // Language name and github token
+    const com = new CommitGetter('Coq', 'coq-community', 'bertrand', 'https://github.com/coq-community/bertrand.git')
+    await com.setup()
+    await com.getCommits()
 }
 
-// async function othermain() {
-    // Language name and github token
-    //const com = new CommitGetter('Lean', 'leanprover-community', 'mathlib', 'https://github.com/leanprover-community/mathlib.git')
-    //await com.setup()
-    //await com.getCommits()
-//}
-
-main()
+othermain()
 
