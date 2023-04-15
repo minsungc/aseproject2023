7a90b7f2ca5237ee6b94cdd3dc716653d22a6ab1
Minsung Cho <minsung@ccs.neu.edu>
2023-04-15 18:49:43 -0400

    small doc edits

--- coq_figures.py
+++ coq_figures.py
@@ -6,119 +6,119 @@ from datetime import datetime
 from matplotlib.dates import DateFormatter
 import os
 
-# Open the JSON file
-with open('DataCollection/CoqTheorems.json') as f:
-    # Load the JSON data into a Python dictionary
-    theorem_data = json.load(f)
+# # Open the JSON file
+# with open('DataCollection/CoqTheorems.json') as f:
+#     # Load the JSON data into a Python dictionary
+#     theorem_data = json.load(f)
 
-#################################
-#### latest activity timeline ###
-#################################
-commits = []
-last_commit_date =  []
-names = []
-for element in theorem_data:
-    if isinstance(element, dict):
-        sources = element['sources'][0]
-        if 'commits' in sources:
-            commit = sources['commits']
-            count = len(commit)
-            commits.append(count)
-            names.append(element['theorem_number'])
-            if len(commit) > 0:
-                last_commit_date.append(commit[len(commit) - 1]['date'])
-    else:
-        print("Each element in the JSON file must be a dictionary.")
-# Convert date strings (e.g. 2014-10-18) to datetime
-dates = [datetime.strptime(d, "%Y-%m-%dT%H:%M:%S%z") for d in last_commit_date]
-# Choose some nice levels
-levels = np.tile([-5, 5, -3, 3, -1, 1],
-                 int(np.ceil(len(dates)/6)))[:len(dates)]
+# #################################
+# #### latest activity timeline ###
+# #################################
+# commits = []
+# last_commit_date =  []
+# names = []
+# for element in theorem_data:
+#     if isinstance(element, dict):
+#         sources = element['sources'][0]
+#         if 'commits' in sources:
+#             commit = sources['commits']
+#             count = len(commit)
+#             commits.append(count)
+#             names.append(element['theorem_number'])
+#             if len(commit) > 0:
+#                 last_commit_date.append(commit[len(commit) - 1]['date'])
+#     else:
+#         print("Each element in the JSON file must be a dictionary.")
+# # Convert date strings (e.g. 2014-10-18) to datetime
+# dates = [datetime.strptime(d, "%Y-%m-%dT%H:%M:%S%z") for d in last_commit_date]
+# # Choose some nice levels
+# levels = np.tile([-5, 5, -3, 3, -1, 1],
+#                  int(np.ceil(len(dates)/6)))[:len(dates)]
 
-# Create figure and plot a stem plot with the date
-fig, ax = plt.subplots(figsize=(14, 4), layout="constrained")
-ax.set(title="Coq theorems latest activity timeline")
+# # Create figure and plot a stem plot with the date
+# fig, ax = plt.subplots(figsize=(14, 4), layout="constrained")
+# ax.set(title="Coq theorems latest activity timeline")
 
-ax.vlines(dates, 0, levels, color="tab:red")  # The vertical stems.
-ax.plot(dates, np.zeros_like(dates), "-o",
-        color="k", markerfacecolor="w")  # Baseline and markers on it.
+# ax.vlines(dates, 0, levels, color="tab:red")  # The vertical stems.
+# ax.plot(dates, np.zeros_like(dates), "-o",
+#         color="k", markerfacecolor="w")  # Baseline and markers on it.
 
-# annotate lines
-for d, l, r in zip(dates, levels, names):
-    ax.annotate(r, xy=(d, l),
-                xytext=(-3, np.sign(l)*3), textcoords="offset points",
-                horizontalalignment="right",
-                verticalalignment="bottom" if l > 0 else "top")
+# # annotate lines
+# for d, l, r in zip(dates, levels, names):
+#     ax.annotate(r, xy=(d, l),
+#                 xytext=(-3, np.sign(l)*3), textcoords="offset points",
+#                 horizontalalignment="right",
+#                 verticalalignment="bottom" if l > 0 else "top")
 
-# format x-axis with 4-month intervals
-ax.xaxis.set_major_locator(mdates.MonthLocator(interval=12))
-ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
-plt.setp(ax.get_xticklabels(), rotation=30, ha="right")
+# # format x-axis with 4-month intervals
+# ax.xaxis.set_major_locator(mdates.MonthLocator(interval=12))
+# ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
+# plt.setp(ax.get_xticklabels(), rotation=30, ha="right")
 
-# remove y-axis and spines
-ax.yaxis.set_visible(False)
-ax.spines[["left", "top", "right"]].set_visible(False)
+# # remove y-axis and spines
+# ax.yaxis.set_visible(False)
+# ax.spines[["left", "top", "right"]].set_visible(False)
 
-ax.margins(y=0.3)
-plt.show()
+# ax.margins(y=0.3)
+# plt.show()
 
-#################################
-#### Commit Counts per Therem ###
-#################################
+# #################################
+# #### Commit Counts per Therem ###
+# #################################
 
-# These are here because of a JSON bug in theorem 96 of Lean.
-# # Plot a histogram of the lengths using matplotlib
-plt.figure(figsize=(15, 3))
-plt.scatter(names, commits)
-plt.suptitle('Coq Commit Counts per Therem\n')
-plt.title('\nTheorem 41 excluded as outlier', fontsize = 6)
-plt.show()
+# # These are here because of a JSON bug in theorem 96 of Lean.
+# # # Plot a histogram of the lengths using matplotlib
+# plt.figure(figsize=(15, 3))
+# plt.scatter(names, commits)
+# plt.suptitle('Coq Commit Counts per Therem\n')
+# plt.title('\nTheorem 41 excluded as outlier', fontsize = 6)
+# plt.show()
 
-#################################
-#### Number of Commits over time ###
-#################################
-dates = []
-# Loop through each element in the list
-for element in theorem_data:
-    # Check if the element is a dictionary
-    if isinstance(element, dict):
-        # Get the length of the dictionary (i.e., the number of key-value pairs)
-        sources = element['sources'][0]
-        if 'commits' in sources:
-            commits = sources['commits']
-            if len(commits) > 0:
-                for commit in commits:
-                    dates.append(datetime.strptime(commit['date'], "%Y-%m-%dT%H:%M:%S%z"))
-    else:
-        # If it's not a dictionary, print an error message
-        print("Each element in the JSON file must be a dictionary.")
-# Count the number of commits per month of each year
-counts = {}
-for date in dates:
-    year_month = date.strftime("%Y-%m")
-    if year_month in counts:
-        counts[year_month] += 1
-    else:
-        counts[year_month] = 1
+# #################################
+# #### Number of Commits over time ###
+# #################################
+# dates = []
+# # Loop through each element in the list
+# for element in theorem_data:
+#     # Check if the element is a dictionary
+#     if isinstance(element, dict):
+#         # Get the length of the dictionary (i.e., the number of key-value pairs)
+#         sources = element['sources'][0]
+#         if 'commits' in sources:
+#             commits = sources['commits']
+#             if len(commits) > 0:
+#                 for commit in commits:
+#                     dates.append(datetime.strptime(commit['date'], "%Y-%m-%dT%H:%M:%S%z"))
+#     else:
+#         # If it's not a dictionary, print an error message
+#         print("Each element in the JSON file must be a dictionary.")
+# # Count the number of commits per month of each year
+# counts = {}
+# for date in dates:
+#     year_month = date.strftime("%Y-%m")
+#     if year_month in counts:
+#         counts[year_month] += 1
+#     else:
+#         counts[year_month] = 1
 
 
-# Sort the counts by year and month
-counts = dict(sorted(counts.items()))
-# Extract the years and months from the keys and values
-x_values = [datetime.strptime(year_month, "%Y-%m") for year_month in counts.keys()]
-y_values = list(counts.values())
+# # Sort the counts by year and month
+# counts = dict(sorted(counts.items()))
+# # Extract the years and months from the keys and values
+# x_values = [datetime.strptime(year_month, "%Y-%m") for year_month in counts.keys()]
+# y_values = list(counts.values())
 
-# Create a timeline plot with the counts
-fig, ax = plt.subplots()
-ax.plot_date(x_values, y_values, fmt="bo-", xdate=True, ydate=False)
+# # Create a timeline plot with the counts
+# fig, ax = plt.subplots()
+# ax.plot_date(x_values, y_values, fmt="bo-", xdate=True, ydate=False)
 
-# Set the x-axis tick labels to show the year and month
-date_formatter = DateFormatter("%Y")
-ax.xaxis.set_major_formatter(date_formatter)
-ax.set_xlabel("Year")
-ax.set_ylabel("Number of Commits")
-ax.set(title= "Coq number of commits per year")
-plt.show()
+# # Set the x-axis tick labels to show the year and month
+# date_formatter = DateFormatter("%Y")
+# ax.xaxis.set_major_formatter(date_formatter)
+# ax.set_xlabel("Year")
+# ax.set_ylabel("Number of Commits")
+# ax.set(title= "Coq number of commits per year")
+# plt.show()
 
 
 #################################
@@ -170,11 +170,13 @@ for date in pull_dates:
     year = date.strftime("%Y")
     if year in pull_counts:
         pull_counts[year] += 1
+        print('incremented')
     else:
         pull_counts[year] = 1
 for date in merge_dates:
     year = date.strftime("%Y")
     if year in merge_counts:
+        print('incremented')
         merge_counts[year] += 1
     else:
         merge_counts[year] = 1
